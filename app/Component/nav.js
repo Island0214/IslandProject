cd1cabc6ee1fd354afd99736f3a52e4528eb65f3
Island0214 <549378813@qq.com>
2017-09-12 19:28:24 +0800

    <feat> <ui> : <增加导航栏>

--- app/Component/nav.js
+++ app/Component/nav.js
@@ -3,17 +3,38 @@ import React, {Component} from 'react'
 import ReactDOM from 'react-dom';
 import config from './config.json';
 import styles from './nav.css';//导入
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from "react-bootstrap";
 
-class Nav extends Component{
+class MyNav extends Component{
     render() {
         return (
-            <ul className="am-nav am-nav-pills am-nav-justify">
-                <li className="am-active"><a href="#">首页</a></li>
-                <li><a href="#">作品</a></li>
-                <li><a href="#">资料</a></li>
-            </ul>
+            <Navbar inverse collapseOnSelect className={styles.bg}>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#" className={styles.menuItem}>ISLAND</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavItem eventKey={1} href="#" className={styles.menuItem}>首页</NavItem>
+                        <NavItem eventKey={2} href="#" className={styles.menuItem}>作品</NavItem>
+                        {/*<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">*/}
+                            {/*<MenuItem eventKey={3.1}>Action</MenuItem>*/}
+                            {/*<MenuItem eventKey={3.2}>Another action</MenuItem>*/}
+                            {/*<MenuItem eventKey={3.3}>Something else here</MenuItem>*/}
+                            {/*<MenuItem divider />*/}
+                            {/*<MenuItem eventKey={3.3}>Separated link</MenuItem>*/}
+                        {/*</NavDropdown>*/}
+                    </Nav>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="#" className={styles.menuItem}>联系</NavItem>
+                        {/*<NavItem eventKey={2} href="#">Link Right</NavItem>*/}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
 
-export default Nav
+export default MyNav
--- app/main.js
+++ app/main.js
@@ -2,9 +2,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 import Greeter from './Component/greeter.js';
-import Nav from './Component/nav.js';
+import MyNav from './Component/nav.js';
 
 import './Component/main.css';//使用require导入css文件
 
-render(<Greeter />, document.getElementById('root'));
-render(<Nav />, document.getElementById('nav'));
+// render(<Greeter />, document.getElementById('root'));
+render(<MyNav />, document.getElementById('nav'));
