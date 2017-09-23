4902f661fd067ead16203e1faccfb1f6c32e448e
Island0214 <549378813@qq.com>
2017-09-23 20:48:37 +0800

    <feat> <js> : <博客列表js新增内容>

---
+++ app/components/js/blogList.js
@@ -0,0 +1,18 @@
+//Greeter,js
+import React, {Component} from 'react'
+import styles from '../css/nav.css';//导入
+import {Navbar, Nav, NavItem, NavDropdown, MenuItem, FormGroup, FormControl} from "react-bootstrap";
+import {Button, Cascader} from 'antd';
+import actions from '../redux/actions/UIFrameAction'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
+
+class BlogList extends Component{
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
